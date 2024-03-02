b7892049642397b10dc89700cf3f2759991ed193
Scott DeVoss <scottdevoss@yahoo.com>
2024-03-01 21:44:18 -0700

    added recipe service test

--- spec/api/v1/recipes_spec.rb
+++ spec/api/v1/recipes_spec.rb
@@ -11,8 +11,29 @@ RSpec.describe "Recipes API", type: :request do
       # }
 
       expect(response).to be_successful
-      # require 'pry'; binding.pry
       expect(response.status).to eq(200)
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json).to be_a(Hash)
+      expect(json).to have_key(:data)
+      expect(json[:data]).to be_a(Array)
+
+      json[:data].each do |recipe|
+        expect(recipe).to have_key(:id)
+        expect(recipe).to have_key(:type)
+        expect(recipe).to have_key(:attributes)
+        expect(recipe[:attributes]).to have_key(:title)
+        expect(recipe[:attributes]).to have_key(:url)
+        expect(recipe[:attributes]).to have_key(:country)
+        expect(recipe[:attributes]).to have_key(:image)
+        expect(recipe[:attributes]).to_not have_key(:count)
+        expect(recipe[:attributes]).to_not have_key(:hits)
+        expect(recipe[:attributes]).to_not have_key(:source)
+        expect(recipe[:attributes]).to_not have_key(:dietLabels)
+        expect(recipe[:attributes]).to_not have_key(:healthLabels)
+        expect(recipe[:attributes]).to_not have_key(:ingredients)
+      end
     end
   end
 end
