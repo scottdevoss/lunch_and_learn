058740762941d2188ec5715ad388c0fdbf7b0aa9
Scott DeVoss <scottdevoss@yahoo.com>
2024-03-03 11:20:52 -0700

    added sad path for US5

--- spec/api/v1/favorites_spec.rb
+++ spec/api/v1/favorites_spec.rb
@@ -33,5 +33,35 @@ RSpec.describe "Favorites", type: :request do
       expect(json).to have_key(:success)
       expect(json[:success]).to eq("Favorite added successfully")
     end
+
+    #Sad Path Adding Favorites
+    it "Will return an error if API is invalid" do
+      user_params = {
+        "name": "Odell",
+        "email": "goodboy@ruffruff.com",
+        "password": "treats4lyf"
+      }
+  
+      user = User.create!(user_params)
+
+      favorite = {
+        "api_key": "wrongapikey",
+        "country": "Mexico",
+        "recipe_link": "https://www.gimmesomeoven.com/best-chicken-enchiladas-ever/",
+        "recipe_title": "Chicken Enchiladas"
+      }
+
+      headers = { "CONTENT_TYPE" => "application/json",
+        "ACCEPT" => "application/json"
+      }
+
+      post "/api/v1/favorites", headers: headers, params: favorite.to_json
+
+
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(response.status).to eq(401)
+      expect(json).to have_key(:error)
+      expect(json[:error]).to eq("Invalid API Key")
+    end
   end
 end
