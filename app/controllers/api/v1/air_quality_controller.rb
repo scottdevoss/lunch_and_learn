ff904afe74630abf62d5d1980c7b77b2d55a0ffd
Scott DeVoss <scottdevoss@yahoo.com>
2024-03-04 09:23:13 -0700

    established faraday connection

---
+++ app/controllers/api/v1/air_quality_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::AirQualityController < ApplicationController
+  def index
+    conn = Faraday.new(url: "https://restcountries.com")
+
+    response = conn.get("/v3.1/name/#{params[:country]}?fullText=true")
+    json = JSON.parse(response.body, symbolize_names: true)
+    require 'pry'; binding.pry
+  end
+end
