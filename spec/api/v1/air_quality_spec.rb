9ae9561c9d83ee0eb9ff1cf4d49b8f2b260f6c78
Scott DeVoss <scottdevoss@yahoo.com>
2024-03-04 11:31:43 -0700

    added test for france

--- spec/api/v1/air_quality_spec.rb
+++ spec/api/v1/air_quality_spec.rb
@@ -25,5 +25,29 @@ RSpec.describe "Air Quality", type: :request do
       expect(json[:data][:attributes]).to have_key(:readable_aqi)
       expect(json[:data][:attributes][:readable_aqi]).to be_a(String)
     end
+
+    it "returns air quality for capital city of France" do
+
+      get "/api/v1/air_quality?country=France"
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json).to have_key(:data)
+      expect(json[:data]).to have_key(:id)
+      expect(json[:data]).to have_key(:type)
+      expect(json[:data]).to have_key(:attributes)
+
+      expect(json[:data][:attributes]).to have_key(:aqi)
+      expect(json[:data][:attributes][:aqi]).to be_a(Integer)
+
+      expect(json[:data][:attributes]).to have_key(:datetime)
+      expect(json[:data][:attributes][:datetime]).to be_a(Integer)
+
+      expect(json[:data][:attributes]).to have_key(:readable_aqi)
+      expect(json[:data][:attributes][:readable_aqi]).to be_a(String)
+    end
   end
 end
