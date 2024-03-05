9540ac00c5f814738de0ec0926faed089cc4eeee
Scott DeVoss <scottdevoss@yahoo.com>
2024-03-05 16:13:03 -0700

    clean up

--- app/poros/video.rb
+++ app/poros/video.rb
@@ -1,8 +1,6 @@
 class Video
   attr_reader :title, :youtube_video_id
   def initialize(video)
-    # @id = nil
-    # @country = country
     @title = video[:snippet][:title]
     @youtube_video_id = video[:id][:videoId]
   end
